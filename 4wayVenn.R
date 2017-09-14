2b669b4fc1b5a6dff6b8e9141b3bbc4283a33d7c
tborrman <tylerborrmant@gmail.com>
2017-09-14 01:13:15 -0400

    myfunctions

--- 4wayVenn.R
+++ 4wayVenn.R
@@ -1,4 +1,5 @@
 library(VennDiagram)
+source('myfunctions.R')
 
 signif <- function(d, cells) {
   for (i in 1:length(cells)) {
@@ -6,34 +7,6 @@ signif <- function(d, cells) {
   }
   return(nrow(d))
 }
-
-clean_qvals <- function(d) {
-  print(nrow(d))
-  # Remove NA cases
-  d <- na.omit(d)
-  print(nrow(d))
-  print(head(d))
-  # Fix zeros
-  d[d$AstrofdrDonut == 0, "AstrofdrDonut"] = 1.0e-42
-  d[d$GMfdrDonut == 0, "GMfdrDonut"] = 1.0e-42
-  d[d$NeufdrDonut == 0, "NeufdrDonut"] = 1.0e-42
-  d[d$NPCfdrDonut == 0, "NPCfdrDonut"] = 1.0e-42
-  print(head(d))
-  # Change q values to log10(qvals)
-  d$AstrofdrDonut <- log10(d$AstrofdrDonut)
-  d$GMfdrDonut <- log10(d$GMfdrDonut)
-  d$NeufdrDonut <- log10(d$NeufdrDonut)
-  d$NPCfdrDonut <- log10(d$NPCfdrDonut)
-  print(head(d))
-  
-  # Remove duplicates
-  d <- d[!duplicated(d[,c('chr1','x1','x2','chr2','y1','y2')]),]
-  print(nrow(d))
-  return(d)
-  
-  
-}
-
 
 
 df <- read.table('../master_loops_Schahram/master_requested_loops', header=TRUE, sep="\t")
