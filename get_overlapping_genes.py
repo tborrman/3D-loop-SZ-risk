2b362c5dbc7642b3fcf37e0df29163172c966415
tborrman <tylerborrman@gmail.com>
2017-09-19 20:22:02 -0400

    add param for loop anchors

--- get_overlapping_genes.py
+++ get_overlapping_genes.py
@@ -7,9 +7,10 @@ import os
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 parser=argparse.ArgumentParser(description='Get genes overlapping loop loci for input cell type')
-parser.add_argument('-g', help= 'Gene table (ex. gene_table)')
-parser.add_argument('-l', help='Loop table (ex. clean_master_requested_loops)')
-parser.add_argument('-c', help='Cell type (Astro, GM, Neu, or NPC )')
+parser.add_argument('-g', help= 'Gene table (ex. gene_table)', type=str, required=True)
+parser.add_argument('-l', help='Loop table (ex. clean_master_requested_loops)', type=str, required=True)
+parser.add_argument('-c', help='Cell type (Astro, GM, Neu, or NPC )', type=str, required=True)
+parser.add_argument('-a', help='use only loop anchors (instead of full loop)', type=bool, default=True)
 args = parser.parse_args()
 
 def counter(i):
@@ -43,7 +44,7 @@ def is_loop_in_cell(line, cell):
 	else:
 		return False
 
-def write_overlap_genes(cell, geneFile, loop_chrom, loop_interval, outfile, gene_set):
+def write_overlap_genes(geneFile, loop_chrom, loop_interval, outfile, gene_set):
 	'''
 	Write genes overlapping loop calls
 	to file
@@ -69,6 +70,8 @@ def write_overlap_genes(cell, geneFile, loop_chrom, loop_interval, outfile, gene
 def main():
 	# Initialize empty gene list
 	genes = set()
+	# Cell type loop counter
+	num_loops_cell = 0
 	# OUTPUT file
 	OUT = open(args.c + '_overlapping_genes.txt', 'w' )
 	with open(args.l, 'r') as l:
@@ -79,15 +82,35 @@ def main():
 			counter(i)
 			# Check if loop exists in cell type
 			if is_loop_in_cell(loop_line, args.c):
-				split_loop_line = loop_line.split('\t')
-				loop_chrom = split_loop_line[0]
-				loop_start = int(split_loop_line[1])
-				loop_stop = int(split_loop_line[5])
-				check_start_below_stop(loop_start, loop_stop)
-				loop_interval = [loop_start, loop_stop]
-				# Parse gene table for overlapping genes
-				genes = write_overlap_genes(args.c, args.g, loop_chrom, loop_interval, OUT, genes)
+				num_loops_cell += 1
+				if args.a:
+					split_loop_line = loop_line.split('\t')
+					loop_chrom = split_loop_line[0]
+					loop_start1 = int(split_loop_line[1])
+					loop_stop1 = int(split_loop_line[2])
+					loop_start2 = int(split_loop_line[4])
+					loop_stop2 = int(split_loop_line[5])
+					check_start_below_stop(loop_start1, loop_stop1)
+					check_start_below_stop(loop_start2, loop_stop2)
+					loop1_interval = [loop_start1, loop_stop1]
+					loop2_interval = [loop_start2, loop_stop2]
+					# Parse gene table for overlapping genes
+					genes = write_overlap_genes(args.g, loop_chrom, loop1_interval, OUT, genes)
+					genes = write_overlap_genes(args.g, loop_chrom, loop2_interval, OUT, genes)
+
+				else:				
+					split_loop_line = loop_line.split('\t')
+					loop_chrom = split_loop_line[0]
+					loop_start = int(split_loop_line[1])
+					loop_stop = int(split_loop_line[5])
+					check_start_below_stop(loop_start, loop_stop)
+					loop_interval = [loop_start, loop_stop]
+					# Parse gene table for overlapping genes
+					genes = write_overlap_genes(args.g, loop_chrom, loop_interval, OUT, genes)
 				
 	OUT.close()
+	print 'Number of loops in '+args.c+': ' + str(num_loops_cell) 
+	print 'Number of genes overlapping loops: '+str(len(genes))
+
 if __name__ == '__main__':
 	main()
