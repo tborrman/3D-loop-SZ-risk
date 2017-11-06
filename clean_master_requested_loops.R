835b989333fa3c0d59fb32ad3516a14409611a55
tborrman <tylerborrman@gmail.com>
2017-11-06 17:15:57 -0500

    downsample cleaning

--- clean_master_requested_loops.R
+++ clean_master_requested_loops.R
@@ -1,8 +1,14 @@
 source('myfunctions.R')
 
-df <- read.table('../master_loops_Schahram/master_requested_loops', header=TRUE, sep="\t")
+df <- read.table('../master_loops_Schahram/full/master_requested_loops', header=TRUE, sep="\t")
 df <- clean_qvals(df)
 
-write.table(df, '../master_loops_Schahram/clean_master_requested_loops', quote=FALSE, 
+write.table(df, '../master_loops_Schahram/full/clean_master_requested_loops', quote=FALSE, 
             sep="\t", row.names=FALSE)
 
+
+df <- read.table('../master_loops_Schahram/downsample/master_requested_loops', header=TRUE, sep="\t")
+df <- clean_qvals(df)
+
+write.table(df, '../master_loops_Schahram/downsample/clean_master_requested_loops', quote=FALSE, 
+            sep="\t", row.names=FALSE)
