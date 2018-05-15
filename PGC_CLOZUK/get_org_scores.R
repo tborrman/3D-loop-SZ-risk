a0122bedd377315d6a57a4a867f42f63e9f65493
tborrman <tylerborrman@gmail.com>
2018-05-15 16:12:16 -0400

    output tables

--- PGC_CLOZUK/get_org_scores.R
+++ PGC_CLOZUK/get_org_scores.R
@@ -6,6 +6,7 @@ args = commandArgs(trailingOnly = TRUE)
 # Organization scores
 df <- read.table(args, sep="\t", header=FALSE)
 colnames(df)[1:4] <- c("chrom", "start", "end", "gene")
+genes <- as.character(df[,4])
 df <- df[5:ncol(df)]
 row.names(df) <- 1:length(df[,1])
 df=t(df)
@@ -21,3 +22,12 @@ myBreaks <- c(seq(-1, 0, length.out=ceiling(100/2) + 1),seq(1/100, 1, length.out
 pheatmap(crnd, color = colorRampPalette(brewer.pal(n = 7, name ="RdBu"))(100), border_color=NA,
          breaks=myBreaks, show_rownames=FALSE, show_colnames=FALSE)
 dev.off()
+
+# Write correlation matrix to file
+outdf <- rbind(genes, crnd)
+rownames(outdf) <- c("", genes)
+
+ write.table(outdf, paste(substr(args, 1, nchar(args) - 4), "_cor_matrix.txt", sep=""), 
+             quote=FALSE, sep="\t", row.names = TRUE, col.names=FALSE)
+ 
+ 
